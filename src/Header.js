beb36666fdc847d03f5636522ed5f477e16121fb
toby <tobyhersey@gmail.com>
2020-09-30 07:54:54 +0100

    logo working alone

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <img 
+              className="header_logo"
+              src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" 
+            />
+        </div>
+    )   
+}
+
+export default Header
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAjHO86eFraW0z6I5O1XuSGdstw_RNqSf0",
+  authDomain: "clone-1cb6d.firebaseapp.com",
+  databaseURL: "https://clone-1cb6d.firebaseio.com",
+  projectId: "clone-1cb6d",
+  storageBucket: "clone-1cb6d.appspot.com",
+  messagingSenderId: "1005340134137",
+  appId: "1:1005340134137:web:d2d481a5e3887d5c33f028",
+  measurementId: "G-64FVCVPTYD"
+};
