ebeeaf88a1914fb994096debc2030740944f563c
toby <tobyhersey@gmail.com>
2020-09-30 16:32:33 +0100

    adding router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
+import Product from "./Product";
+import { BrowserRouter as Router , Switch , Route} from "react-router-dom";
+
 
 function App() {
   return (
     // BEM
-    <div className="app">
-      <Header />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Home />
+        <Product />
+      </div>
+    </Router>
   );
 }
 
