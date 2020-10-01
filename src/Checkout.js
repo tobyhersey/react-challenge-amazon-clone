2cd31e54f0f30dfcdc9dbdbb2abe650b6832f7d2
toby <tobyhersey@gmail.com>
2020-10-01 12:42:45 +0100

    working with remove basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,28 +1,40 @@
 import React from "react"
 import "./Checkout.css"
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
 
 function Checkout() {
-    return (
-        <div className="Checkout">;
-            <div className="checkout__left">
-            <img
-                className="checkout__ad"
-                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                alt=""
-            />
-            <div>
-                <h2 className="checkout__title">Your Shopping Basket</h2>
+  const [{ basket } , dispatch] = useStateValue();
+  return (
+    <div className="Checkout">;
+      <div className="checkout__left">
+      <img
+          className="checkout__ad"
+          src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+          alt=""
+      />
+      <div>
+          <h2 className="checkout__title">Your Shopping Basket</h2>
 
-            </div>
-            <div>
-                <h2 className="checkout__right"></h2>
-                < Subtotal />
-                <h2> Subtotal</h2>
-            </div>
-            </div>
-        </div>
-    )
+            {basket.map(item => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />  
+            ))}
+      </div>
+      <div>
+          <h2 className="checkout__right"></h2>
+          < Subtotal />
+          <h2> Subtotal</h2>
+      </div>
+      </div>
+  </div>
+  )
 }
 
 export default Checkout
