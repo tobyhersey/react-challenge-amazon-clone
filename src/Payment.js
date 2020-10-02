394c0029cb77a9c26b4e544fe813c0116f664ea4
toby <tobyhersey@gmail.com>
2020-10-02 08:32:40 +0100

    stripe payment added UI only

--- src/Payment.js
+++ src/Payment.js
@@ -1,5 +1,5 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import Axios from 'axios';
+import Axios from './axios';
 import React, { useEffect } from 'react'
 import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
